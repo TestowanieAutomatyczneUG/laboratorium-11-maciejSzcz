4f4a37bfd0b618d7de8902b73e6772b85cc3c654
MaciejSzczesny <yetek.lol@gmail.com>
2020-12-16 18:40:56 +0100

    added tests for notes_storage

---
+++ src/zad2/notes_storage.py
@@ -0,0 +1,12 @@
+class NotesStorage(object):
+    def __init__(self):
+        self.notes = []
+
+    def add(self, note):
+        pass
+
+    def clear(self):
+        pass
+
+    def getAllNotesOf(self, name):
+        pass
